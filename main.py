90e3c723cafb485be0e3ada5e9784f4febae2143
sibonisot <sibonisot@yahoo.com>
2025-04-01 19:17:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.saucedemo.com/")
+
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+
+driver.find_element(By.XPATH, "//*[@id='header_container']/div[2]/span").is_displayed()
+
+ProductText = driver.find_element(By.XPATH, "//*[@id='header_container']/div[2]/span").text
+
+if ProductText=="Products":
+    print(" Test has passed")
+    assert True
+
+else:
+    print(" Test has Failed")
+    assert False
+
+time.sleep(5)
