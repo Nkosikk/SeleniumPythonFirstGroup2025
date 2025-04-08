f5c55f6be9b9cd03c84d1b4c539cbf5788bd4e33
kgaogelo0719 <kgaogelomaluleke@gmail.com>
2025-04-08 18:46:07 +0200

    Added click cart icon method

--- Pages/landingPage.py
+++ Pages/landingPage.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class LandingPage:
     title_ProductText_xpath = "//*[@id='header_container']/div[2]/span"
     button_addToCart_id = "add-to-cart-sauce-labs-backpack"
+    button_cart_xpath = "//*[@id='shopping_cart_container']/a"
 
     def __init__(self, driver):
         self.driver = driver
@@ -18,3 +19,7 @@ class LandingPage:
     def clickAddToCart(self):
      element = self.driver.find_element(By.ID, self.button_addToCart_id)
      element.click()
+
+    def verifyAddToCart(self):
+        element = self.driver.find_element(By.XPATH, self.button_cart_xpath)
+        element.click()
