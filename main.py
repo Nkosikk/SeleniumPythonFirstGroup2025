aedb1af2b0af778c9b5120fada18239ecd6d57a7
LNtanjana <lindantanjana@gmail.com>
2025-04-09 18:08:05 +0200

    Added code on main to complete checkout

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ driver.find_element(By.ID, "login-button").click()
 driver.find_element(By.XPATH, "//*[@id='header_container']/div[2]/span").is_displayed()
 
 ProductText = driver.find_element(By.XPATH, "//*[@id='header_container']/div[2]/span").text
-addToCartButton = driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
-clickCart = driver.find_element(By.XPATH, "//*[@id='shopping_cart_container']/a")
-
 if ProductText=="Products":
     print(" Test has passed")
     assert True
@@ -23,5 +20,28 @@ if ProductText=="Products":
 else:
     print(" Test has Failed")
     assert False
+driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.XPATH, "//*[@id='shopping_cart_container']/a").click()
+ProductText = driver.find_element(By.XPATH, "//span[@class='title'][contains(.,'Your Cart')]").text
+if ProductText=="Your Cart":
+    print(" Test has passed")
+    assert True
+else:
+    print(" Test has Failed")
+    assert False
+driver.find_element(By.ID,  "checkout").click()
+driver.find_element(By.ID, "first-name").send_keys("Linda")
+driver.find_element(By.ID, "last-name").send_keys("Ntanjana")
+driver.find_element(By.ID, "postal-code").send_keys("7850")
+driver.find_element(By.XPATH, "//input[@id='continue']").click()
+ProductText = driver.find_element(By.XPATH, "//span[@class='title'][contains(.,'Checkout: Overview')]").text
+if ProductText=="Checkout: Overview":
+    print(" Test has passed")
+    assert True
+else:
+    print(" Test has Failed")
+    assert False
+driver.find_element(By.XPATH, "//button[@id='finish']").click()
+
 
 time.sleep(5)
