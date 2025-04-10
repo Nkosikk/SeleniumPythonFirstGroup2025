595831ae7ab1b8229efd35230b851e20297b28ca
Fifi-zulu <unicerzulu@gmail.com>
2025-04-10 18:49:51 +0200

    Fixed the browser

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -3,16 +3,16 @@ import time
 from selenium import webdriver
 
 def setup(browser):
-    if browser.lower == 'chrome':
+    if browser.lower() == 'chrome':
         driver = webdriver.Chrome()
         time.sleep(5)
 
-    elif browser.lower == 'edge':
+    elif browser.lower() == 'edge':
         driver = webdriver.Edge()
 
     else:
         driver = webdriver.Safari()
     return driver
 
-setup("chrome")
+setup("edge")
 
