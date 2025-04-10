15a3d24e7d80daaf13ee3e989251842cd7003be9
Mahlatse03 <mahlatsed@gmail.com>
2025-04-10 19:19:51 +0200

    Merge remote-tracking branch 'origin/main' into MahlatseD

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,18 +1,25 @@
 import time
 
+import pytest
 from selenium import webdriver
 
+@pytest.fixture
 def setup(browser):
-    if browser.lower == 'chrome':
+    if browser.lower() == 'chrome':
         driver = webdriver.Chrome()
         time.sleep(5)
 
-    elif browser.lower == 'edge':
+    elif browser.lower() == 'edge':
         driver = webdriver.Edge()
 
     else:
         driver = webdriver.Safari()
     return driver
 
-setup("chrome")
 
+def pytest_adoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture
+def browser(request):
+    return request.config.getoption("--browser")
