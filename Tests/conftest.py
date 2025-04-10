3c8d3172ff3b4d58572d337c287e5b3d0b6b3b0f
Nkosi <njceles@gmail.com>
2025-04-10 19:10:35 +0200

    completed the conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,8 +1,9 @@
 import time
 
+import pytest
 from selenium import webdriver
 
-
+@pytest.fixture
 def setup(browser):
     if browser.lower() == 'chrome':
         driver = webdriver.Chrome()
@@ -19,5 +20,6 @@ def setup(browser):
 def pytest_adoption(parser):
     parser.addoption("--browser")
 
+@pytest.fixture
 def browser(request):
     return request.config.getoption("--browser")
